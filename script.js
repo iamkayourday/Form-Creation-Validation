4b7f2814f928ae9d78aafdfc394833811a96f3ed
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-07-17 03:45:48 +0100

    Addedfunction

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DOMContentLoadedEvent
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback')
 
